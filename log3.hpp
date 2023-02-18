06e97823b10bba51734ca15888659fd6f1c7587e
beromer <beromer@fastmail.com>
2023-02-17 21:31:21 -0700

    add colorization and all logging levels

--- log3.hpp
+++ log3.hpp
@@ -7,16 +7,151 @@
 #include <iomanip>
 #include <fmt/core.h>
 #include <fmt/chrono.h>
+#include <fmt/color.h>
 
 struct Log3{
+
+private:
+    inline static std::string msgFormat{"[{}] {:>6}: {}\n"};
+    inline static std::string msgFormatR{"[{}] {:>6}: ({}) {}\n"};
+    // Basic Functions
     static std::string getTime(){
         std::time_t time = std::time(nullptr);
         return fmt::format("{:%Y-%m-%d %H:%M:%S}",fmt::localtime(time));
     }
+
+    static void vlog(fmt::text_style style, const std::string& levelName, fmt::string_view format, fmt::format_args args){
+        if (colorable)
+            fmt::print(style,msgFormat,getTime(),levelName,fmt::vformat(format,args));
+        else
+            fmt::print(msgFormat,getTime(),levelName,fmt::vformat(format,args));
+        std::cout << std::flush;
+    }
+
+    static void vlog(const std::string& levelName, fmt::string_view format, fmt::format_args args){
+        fmt::print(msgFormat,getTime(),levelName,fmt::vformat(format,args));
+        std::cout << std::flush;
+    }
+
+    static void vlogRank(fmt::text_style style, const std::string& levelName, fmt::string_view format, fmt::format_args args){
+        if (colorable)
+            fmt::print(style, msgFormatR,getTime(),levelName,rank,fmt::vformat(format,args));
+        else
+            fmt::print(msgFormatR,getTime(),levelName,rank,fmt::vformat(format,args));
+        std::cout << std::flush;
+    }
+
+    static void vlogRank(const std::string& levelName, fmt::string_view format, fmt::format_args args){
+        fmt::print(msgFormatR,getTime(),levelName,rank,fmt::vformat(format,args));
+        std::cout << std::flush;
+    }
+
+public:
+    // properties
     inline static int rank = 0;
-    template<typename T>
-    static void info(T message){
-        std::cout << fmt::format("[{}] INFO: {}",getTime(),message) << std::endl;
+    inline static int verbosity = 5;
+    inline static bool colorable = true;
+
+    // Error Logging Functions
+    template<typename S, typename... Args>
+    static void error(const S& format, Args&&... args){
+        if (rank !=0) return;
+        if (verbosity < 1) return;
+        vlog(fmt::emphasis::bold | fmt::fg(fmt::color::red),"ERROR",format, fmt::make_format_args(args...));
+    }
+    template<typename S, typename... Args>
+    static void errorAll(const S& format, Args&&... args){
+        if (verbosity < 1) return;
+        vlogRank(fmt::emphasis::bold | fmt::fg(fmt::color::red),"ERROR",format, fmt::make_format_args(args...));
+    }
+    template<typename S, typename... Args>
+    static void errorRank(const int r, const S& format, Args&&... args){
+        if (rank !=r) return;
+        if (verbosity < 1) return;
+        vlogRank(fmt::emphasis::bold | fmt::fg(fmt::color::red),"ERROR",format, fmt::make_format_args(args...));
+    }
+
+    // Warning Logging Functions
+    template<typename S, typename... Args>
+    static void warning(const S& format, Args&&... args){
+        if (rank !=0) return;
+        if (verbosity < 2) return;
+        vlog(fmt::emphasis::bold | fmt::fg(fmt::color::yellow),"WARNING",format, fmt::make_format_args(args...));
+    }
+    template<typename S, typename... Args>
+    static void warningAll(const S& format, Args&&... args){
+        if (verbosity < 2) return;
+        vlogRank(fmt::emphasis::bold | fmt::fg(fmt::color::yellow),"WARNING",format, fmt::make_format_args(args...));
+    }
+    template<typename S, typename... Args>
+    static void warningRank(const int r, const S& format, Args&&... args){
+        if (rank !=r) return;
+        if (verbosity < 2) return;
+        vlogRank(fmt::emphasis::bold | fmt::fg(fmt::color::yellow),"WARNING",format, fmt::make_format_args(args...));
+    }
+
+    // Message Logging Functions
+    template<typename S, typename... Args>
+    static void message(const S& format, Args&&... args){
+        if (rank !=0) return;
+        if (verbosity < 3) return;
+        vlog(fmt::emphasis::bold | fmt::fg(fmt::color::blue),"MESSAGE",format, fmt::make_format_args(args...));
+    }
+    template<typename S, typename... Args>
+    static void messageAll(const S& format, Args&&... args){
+        if (verbosity < 3) return;
+        vlogRank(fmt::emphasis::bold | fmt::fg(fmt::color::blue),"MESSAGE",format, fmt::make_format_args(args...));
+    }
+    template<typename S, typename... Args>
+    static void messageRank(const int r, const S& format, Args&&... args){
+        if (rank !=r) return;
+        if (verbosity < 3) return;
+        vlogRank(fmt::emphasis::bold | fmt::fg(fmt::color::blue),"MESSAGE",format, fmt::make_format_args(args...));
+    }
+
+    // Info Logging functions
+    template<typename S, typename... Args>
+    static void info(const S& format, Args&&... args){
+        if (rank !=0) return;
+        if (verbosity < 4) return;
+        vlog("INFO",format, fmt::make_format_args(args...));
+    }
+    template<typename S, typename... Args>
+    static void infoAll(const S& format, Args&&... args){
+        if (verbosity < 4) return;
+        vlogRank("INFO",format, fmt::make_format_args(args...));
+    }
+    template<typename S, typename... Args>
+    static void infoRank(const int r, const S& format, Args&&... args){
+        if (rank !=r) return;
+        if (verbosity < 4) return;
+        vlogRank("INFO",format, fmt::make_format_args(args...));
+    }
+
+    // Debug Logging Functions
+    template<typename S, typename... Args>
+    static void debug(const S& format, Args&&... args){
+        if (rank !=0) return;
+        if (verbosity < 5) return;
+        vlog(fmt::emphasis::bold | fmt::fg(fmt::color::purple),"DEBUG",format, fmt::make_format_args(args...));
+    }
+    template<typename S, typename... Args>
+    static void debugAll(const S& format, Args&&... args){
+        if (verbosity < 5) return;
+        vlogRank(fmt::emphasis::bold | fmt::fg(fmt::color::purple),"DEBUG",format, fmt::make_format_args(args...));
+    }
+    template<typename S, typename... Args>
+    static void debugRank(const int r, const S& format, Args&&... args){
+        if (rank !=r) return;
+        if (verbosity < 5) return;
+        vlogRank(fmt::emphasis::bold | fmt::fg(fmt::color::purple),"DEBUG",format, fmt::make_format_args(args...));
+    }
+
+    // Banner Logging Functions
+    template<typename S, typename... Args>
+    static void banner(const S& format, Args&&... args){
+        if (rank !=0) return;
+        fmt::print(fmt::emphasis::bold | fmt::fg(fmt::color::green),"[{}] {}",getTime(),fmt::vformat(format, fmt::make_format_args(args...)));
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -2,9 +2,53 @@
 #include "log3.hpp"
 
 int main() {
+    std::string name{"Brian"};
     Log3::rank = 0;
-    Log3::info(std::string("Hello!"));
-    const auto me = []{return Log3::rank+1;}();
-    Log3::info(me);
+    Log3::colorable = true;
+    Log3::verbosity = 5;
+    Log3::info("Hello {} from rank {}!",name,Log3::rank);
+    Log3::error("Can't find rank {}!",Log3::rank);
+    Log3::errorRank(0,"Can't find rank {}!",Log3::rank);
+    Log3::errorAll("Can't find rank {}!",Log3::rank);
+    Log3::warning("Can't find rank {}!",Log3::rank);
+    Log3::warningRank(0,"Can't find rank {}!",Log3::rank);
+    Log3::warningAll("Can't find rank {}!",Log3::rank);
+    Log3::message("Can't find rank {}!",Log3::rank);
+    Log3::messageRank(0,"Can't find rank {}!",Log3::rank);
+    Log3::messageAll("Can't find rank {}!",Log3::rank);
+    Log3::debug("Can't find rank {}!",Log3::rank);
+    Log3::debugRank(0,"Can't find rank {}!",Log3::rank);
+    Log3::debugAll("Can't find rank {}!",Log3::rank);
+
+    Log3::rank = 1;
+    Log3::colorable = true;
+    Log3::verbosity = 5;
+    Log3::info("Hello {} from rank {}!",name,Log3::rank);
+    Log3::infoAll("Hello from non-zero rank!");
+    Log3::error("Can't find rank {}!",Log3::rank);
+
+    Log3::rank = 0;
+    Log3::colorable = false;
+    Log3::verbosity = 5;
+    Log3::info("Hello {} from rank {}!",name,Log3::rank);
+    Log3::error("Can't find rank {}!",Log3::rank);
+
+    Log3::rank = 0;
+    Log3::colorable = true;
+    Log3::verbosity = 1;
+    Log3::info("Hello {} from rank {}!",name,Log3::rank);
+    Log3::error("Can't find rank {}!",Log3::rank);
+
+    Log3::rank = 3;
+    Log3::colorable = true;
+    Log3::verbosity = 5;
+    Log3::info("Hello {} from rank {}!",name,Log3::rank);
+    Log3::infoRank(3,"Testing Rank 3 '{}'",name);
+    Log3::infoRank(5,"Testing Rank 5 '{}'",name);
+    Log3::error("Can't find rank {}!",Log3::rank);
+
+    Log3::rank = 0;
+    Log3::banner("Logging Complete!");
+
     return 0;
 }
